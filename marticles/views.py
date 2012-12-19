dc1bad6a6a5ecf875ac48e191817b0c3ef34e202
Atlantic777 <atlantic777@lugons.org>
2012-12-19 12:01:59 +0100

    Fixed CRLF bug.

--- marticles/views.py
+++ marticles/views.py
@@ -31,8 +31,11 @@ def submit_edit(request):
 	subprocess.call(['rm', '-rf', '/tmp/temp-repo'])
 	subprocess.call(['git', 'clone', '/home/nikola/sajt/lugons-wiki', '/tmp/temp-repo'])
 	f = open('/tmp/temp-repo/the_repo/lugons.md', 'w')
-	f.write(request.POST.get('q').encode('UTF-8'))
+	tmp_string = request.POST.get('q').replace('\r', '').encode('UTF-8')
+	f.write(tmp_string)
 	f.close()
+
+	print tmp_string
 
 	args = ['git', 'commit', '-a', '-m', "Some commit"]
 	git = subprocess.Popen(args, cwd='/tmp/temp-repo', 
@@ -45,6 +48,5 @@ def submit_edit(request):
 				stdout=subprocess.PIPE, 
 				stderr=subprocess.PIPE)
 	(out, err) = git.communicate()
-	subprocess.call(['rm', '-rf', '/tmp/temp-repo'])
 
 	return render_to_response('patch.html', {'patch':out}) 
