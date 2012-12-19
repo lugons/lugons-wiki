033340d7e32b9b24d3f495f530e77052d439a078
Atlantic777 <atlantic777@lugons.org>
2012-12-19 02:06:12 +0100

    Added some submit options.

--- marticles/views.py
+++ marticles/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render_to_response
 from django import forms
 import os, subprocess
 from django.http import HttpResponse
+from django.template import RequestContext
 
 repo = 'the_repo/'
 
@@ -25,21 +26,25 @@ def edit(request, filename):
 	return render_to_response('editor.html',{'stuff':string} )
 
 def submit_edit(request):
-	if 'q' in request.GET:
-		subprocess.call(['rm', '-rf', '/tmp/temp-repo'])
-		subprocess.call(['git', 'clone', '/home/nikola/sajt/lugons-wiki', '/tmp/temp-repo'])
-		f = open('/tmp/temp-repo/the_repo/lugons.md', 'w')
-		f.write(str(request.GET['q']))
-		f.close()
-		subprocess.call(['git', 'commit', '-a', '-m', "Some commit"])
-	
-		args = ['git', 'format-patch', 'origin/master', '--stdout']
-		git = subprocess.Popen(args, cwd='/tmp/temp-repo', 
-					stdout=subprocess.PIPE, 
-					stderr=subprocess.PIPE)
-		(out, err) = git.communicate()
-		
-	else:
-		message = 'Empty form.'
+	c = RequestContext(request)
 
-	return HttpResponse(out)
+	subprocess.call(['rm', '-rf', '/tmp/temp-repo'])
+	subprocess.call(['git', 'clone', '/home/nikola/sajt/lugons-wiki', '/tmp/temp-repo'])
+	f = open('/tmp/temp-repo/the_repo/lugons.md', 'w')
+	f.write(c)
+	f.close()
+
+	args = ['git', 'commit', '-a', '-m', "Some commit"]
+	git = subprocess.Popen(args, cwd='/tmp/temp-repo', 
+				stdout=subprocess.PIPE, 
+				stderr=subprocess.PIPE)
+	(out, err) = git.communicate()
+
+	args = ['git', 'format-patch', 'origin/master', '--stdout']
+	git = subprocess.Popen(args, cwd='/tmp/temp-repo', 
+				stdout=subprocess.PIPE, 
+				stderr=subprocess.PIPE)
+	(out, err) = git.communicate()
+	subprocess.call(['rm', '-rf', '/tmp/temp-repo'])
+
+	return HttpResponse("<html><body><pre>"+out+"</pre></body></html>")
