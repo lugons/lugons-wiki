7075d24e32948a9a9d83f3639cd395a9f41af914
Atlantic777 <atlantic777@lugons.org>
2012-12-21 00:27:43 +0100

    Separated make_patch() and added the django form

--- marticles/views.py
+++ marticles/views.py
@@ -7,6 +7,9 @@ from django.template import RequestContext
 
 repo = 'the_repo/'
 
+class EditForm(forms.Form):
+	text = forms.CharField(widget=forms.Textarea)
+
 def hello(request, filename):
 	try:
 		f = open(repo+filename+'.md')
@@ -23,30 +26,40 @@ def edit(request, filename):
 		return render_to_response('hello.html', {'stuff' : "#fajl ne posoji"})
 	string = f.read()
 	f.close()
-	return render_to_response('editor.html',{'stuff':string},  context_instance=RequestContext(request) )
 
-def submit_edit(request):
-	c = RequestContext(request)
+	if request.method == 'POST':
+		form = EditForm(request.POST)
+		form.is_valid()
+		out = make_patch(filename, form.clean()['text'])
+		return render_to_response('patch.html', {'patch':out}) 
+	else:
+		form = EditForm({'text':string},request.POST)
 
+	return render_to_response('editor.html',{'form':form}, context_instance=RequestContext(request))
+
+def make_patch(filename, data):
+	#Clean and clone repo
 	subprocess.call(['rm', '-rf', '/tmp/temp-repo'])
 	subprocess.call(['git', 'clone', '/home/nikola/sajt/lugons-wiki', '/tmp/temp-repo'])
-	f = open('/tmp/temp-repo/the_repo/lugons.md', 'w')
-	tmp_string = request.POST.get('q').replace('\r', '').encode('UTF-8')
-	f.write(tmp_string)
+
+	#Write new data into file
+	f = open('/tmp/temp-repo/the_repo/'+filename+'.md', 'w')
+	f.write(data.replace('\r', '').encode('UTF-8'))
 	f.close()
 
-	print tmp_string
-
+	#Commit edit
 	args = ['git', 'commit', '-a', '-m', "Some commit"]
 	git = subprocess.Popen(args, cwd='/tmp/temp-repo', 
 				stdout=subprocess.PIPE, 
 				stderr=subprocess.PIPE)
 	(out, err) = git.communicate()
 
+	#Create format patch
 	args = ['git', 'format-patch', 'origin/master', '--stdout']
 	git = subprocess.Popen(args, cwd='/tmp/temp-repo', 
 				stdout=subprocess.PIPE, 
 				stderr=subprocess.PIPE)
 	(out, err) = git.communicate()
 
-	return render_to_response('patch.html', {'patch':out}) 
+	return out
+
