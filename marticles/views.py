8b0d1af49a853ece8355151efb7a38f5c78e904d
Atlantic777 <atlantic777@lugons.org>
2012-12-21 01:01:34 +0100

    Added randomness in make_patch()
    Every patch is generated in temp repo clone and name of that git repo
    is random.

--- marticles/views.py
+++ marticles/views.py
@@ -4,6 +4,7 @@ from django import forms
 import os, subprocess
 from django.http import HttpResponse
 from django.template import RequestContext
+import string, random
 
 repo = 'the_repo/'
 
@@ -38,31 +39,33 @@ def edit(request, filename):
 	return render_to_response('editor.html',{'form':form}, context_instance=RequestContext(request))
 
 def make_patch(filename, data):
+	#Generate random string
+	rs = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(8))
+
 	#Clean and clone repo
-	subprocess.call(['rm', '-rf', '/tmp/temp-repo'])
-	subprocess.call(['git', 'clone', '/home/nikola/sajt/lugons-wiki', '/tmp/temp-repo'])
+	subprocess.call(['git', 'clone', '/home/nikola/sajt/lugons-wiki', '/tmp/temp-repo-'+rs])
 
 	#Write new data into file
-	f = open('/tmp/temp-repo/the_repo/'+filename+'.md', 'w')
+	f = open('/tmp/temp-repo-'+rs+'/the_repo/'+filename+'.md', 'w')
 	f.write(data.replace('\r', '').encode('UTF-8'))
 	f.close()
 
 	#Commit edit
 	args = ['git', 'commit', '-a', '-m', "Some commit"]
-	git = subprocess.Popen(args, cwd='/tmp/temp-repo', 
+	git = subprocess.Popen(args, cwd='/tmp/temp-repo-'+rs, 
 				stdout=subprocess.PIPE, 
 				stderr=subprocess.PIPE)
 	(out, err) = git.communicate()
 
 	#Create format patch
 	args = ['git', 'format-patch', 'origin/master', '--stdout']
-	git = subprocess.Popen(args, cwd='/tmp/temp-repo', 
+	git = subprocess.Popen(args, cwd='/tmp/temp-repo-'+rs, 
 				stdout=subprocess.PIPE, 
 				stderr=subprocess.PIPE)
 	(out, err) = git.communicate()
 
 	#Remove temp repo
-	subprocess.call(['rm', '-rf', '/tmp/temp-repo'])
+	subprocess.call(['rm', '-rf', '/tmp/temp-repo-'+rs])
 
 	return out
 
